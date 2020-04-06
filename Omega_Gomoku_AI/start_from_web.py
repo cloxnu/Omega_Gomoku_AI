b503cf3871204249d647c07287f6bfefdaf4b435
CLOX <o18089541818@outlook.com>
2020-04-06 20:25:52 +0800

    Update start_from_web.py

--- Omega_Gomoku_AI/start_from_web.py
+++ Omega_Gomoku_AI/start_from_web.py
@@ -4,7 +4,7 @@ print("Web 服务器已开启于 http://127.0.0.1:5000. 按 <Ctrl-C> 关闭")
 print("The web server has been started at http://127.0.0.1:5000. Press <Ctrl-C> to close")
 
 try:
-    web_start.socket_io.run(app)
+    web_start.socket_io.run(app, host='0.0.0.0')
     # app.run(host='0.0.0.0')
 except KeyboardInterrupt:
     # web_start.disconnect()
